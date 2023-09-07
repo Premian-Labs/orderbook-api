1e0e9fc686c170f961228559f751e597db2806d3
Guceri <31142240+Guceri@users.noreply.github.com>
2023-09-07 19:49:28 -0400

    I am OCD

--- test/helpers/util.ts
+++ test/helpers/util.ts
@@ -3,9 +3,9 @@ import { URLSearchParams } from 'url'
 
 // verified to exist in checkConfig
 const port = process.env.HTTP_PORT!
-type requestType =  'GET' | 'POST' | 'PATCH' | 'DELETE'
+type RequestType =  'GET' | 'POST' | 'PATCH' | 'DELETE'
 
-export async function apiRequest(method: requestType, endpoint: string, data?: any, queryParams?: any) {
+export async function apiRequest(method: RequestType, endpoint: string, data?: any, queryParams?: any) {
 	const url = `https://localhost:${port}/${endpoint}`
 	switch (method){
 		case 'GET':{
