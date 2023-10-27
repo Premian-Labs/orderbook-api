fc0cc484b768cf9d8e3858c1d6ca7d7d8cbc3ff6
nikkatalnikov <nk@flowtale.ai>
2023-10-27 16:03:10 +0100

    e2e: websockets initial

---
+++ test/ws.ts
@@ -0,0 +1,44 @@
+import { expect } from 'chai'
+import { WebSocket } from 'ws'
+import { checkEnv } from '../src/config/checkConfig'
+
+// NOTE: integration tests can only be run on development mode & with testnet credentials
+checkEnv(true)
+
+const url = `ws://localhost:${process.env.HTTP_PORT}`
+const wsConnection = new WebSocket(url)
+
+function delay(ms: number) {
+	return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+describe('test WS connectivity', () => {
+	it('should connect to ws', async () => {
+		let infoMessage = ''
+		const authMsg = {
+			type: 'AUTH',
+			apiKey: process.env.TESTNET_ORDERBOOK_API_KEY,
+			body: null,
+		}
+
+		wsConnection.on('message', (data) => {
+			const message = JSON.parse(data.toString())
+			switch (message.type) {
+				case 'INFO': {
+					infoMessage = message.message
+					break
+				}
+				default: {
+					throw `Wrong message type ${message.type}`
+				}
+			}
+		})
+
+		await delay(500)
+		expect(wsConnection.readyState === wsConnection.OPEN).to.be.true
+
+		wsConnection.send(JSON.stringify(authMsg))
+		await delay(500)
+		expect(infoMessage).to.eq(`Session authorized. Subscriptions enabled.`)
+	})
+})
