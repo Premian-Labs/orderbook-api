3eaccdc6d1635fa782baea94464a639f748a080d
nikkatalnikov <nk@flowtale.ai>
2023-12-09 21:03:07 +0000

    gasLimit 10_000_000

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -20,7 +20,7 @@ export const apiKey =
 		? process.env.MAINNET_ORDERBOOK_API_KEY
 		: process.env.TESTNET_ORDERBOOK_API_KEY
 
-export const gasLimit: number = 1400000
+export const gasLimit: number = 10_000_000
 
 export const referralAddress = ZeroAddress
 export const rpcUrl =
